0d0dfbe6b5afc99182417b7e92689852c427156f
Hiiro Miyano <hiiro.miyano@gmail.com>
2022-06-30 14:48:17 +0900

    店舗とメニュー情報用のファクトリー追加

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Menu;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class MenuFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Menu::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'introduction' => fake()->realText(),
+            'price' => fake()->randomNumber(),
+            'cost' => fake()->randomNumber(),
+        ];
+    }
+
+}
