ddcd4eea6d0b3d363ff8bf4ab99249f9929504ab
Hiiro Miyano <hiiro.miyano@gmail.com>
2022-06-30 14:41:19 +0900

    サンプル用のモデルなどを追加

---
+++ database/migrations/1970_01_01_000001_create_menus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('shop_id');
+            $table->string('name');
+            $table->text('introduction')->nullable(true);
+            $table->integer('price');
+            $table->integer('cost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+};
